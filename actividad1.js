24218363d6b3fe0b23c63faad6ec43ec00465fca
TsantiG <santigutierrez081@gmail.com>
2024-08-03 11:34:05 -0500

    Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por TsantiG

--- actividad1.js
+++ actividad1.js
@@ -6,9 +6,9 @@ class persona{
     /* paque no se te olvide, los metodos de restrincion se ejecutan en el constructor para evaluar los datos del objeto que se va a agregar.
     y se hace otro metodo para obtener o mostrar  el dato de salida */
 
-    constructor(nombre){
+    constructor(nombre, edad){
         this.establecerNombre(nombre);
-        // this.establecerEdad(edad);
+        this.establecerEdad(edad);
         // this.establecerFechaNacimiento(fechaNacimiento);
     }
 
@@ -23,7 +23,23 @@ class persona{
     obtenernombre(){
         return this.#nombre;
     }
+
+    establecerEdad(edad){
+        if (typeof edad === "number" && edad > 0){
+            this.#edad = edad;
+        }else{
+            console.log("Ingrese una edad valida")
+        }
+    }
+
+    obtenerEdad(){
+        return this.#edad;
+    }
+
+
 }
 
-const santi = new persona("santiago")
-console.log(santi.obtenernombre())
+const santi = new persona("santiago", 23);
+console.log(santi.obtenernombre());
+
+console.log(santi.obtenerEdad());
