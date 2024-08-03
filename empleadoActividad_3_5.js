1077d7f065eca8dfbe36f71b44383ab89e434f87
TsantiG <santigutierrez081@gmail.com>
2024-08-03 15:10:03 -0500

    Herencia y Polimorfismo - Implementación de Clase Empleado por TsantiG

--- empleadoActividad_3_5.js
+++ empleadoActividad_3_5.js
@@ -1,10 +1,22 @@
 const Trabajador = require('./interfaceActividad3');
 
 class Empleado extends Trabajador{
+
     realizarTarea() {
         console.log("Trabajando en la oficina.");
     }
 
+    #nombre;
+    #apellido;
+    #salario;
+    #numEmpleado;
 
-    
+    constructor(nombre, apellido, salario, numEmpleado){
+        this.#nombre = nombre;
+        this.#apellido = apellido;
+        this.#salario = salario;
+        this.#numEmpleado = numEmpleado;
+    }
+
+
 }
