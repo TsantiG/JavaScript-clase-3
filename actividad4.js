f2400792c1049de93a34c4b1253d6e63a9f44cca
TsantiG <santigutierrez081@gmail.com>
2024-08-03 14:18:08 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por TsantiG

--- actividad4.js
+++ actividad4.js
@@ -2,6 +2,30 @@ class FiguraGeometrica {
     #nombreFigura = " ";
     #colorFigura = "";
     constructor(nombreFigura, colorFigura){
-        
+        this.establecerNombre(nombreFigura);
+        this.establecerColor(colorFigura);
     }
+
+    establecerNombre(nombreFigura){
+        this.#nombreFigura = nombreFigura;
+    }
+
+    obtenerNombre(){
+        return this.#nombreFigura;
+    }
+
+    establecerColor(colorFigura){
+        this.#colorFigura = colorFigura;
+    }
+
+    obtenerColor(){
+        return this.#colorFigura;
+    }
+
+
 }
+const circulo = new FiguraGeometrica("Circulo", "Rojo");
+
+console.log(circulo.obtenerNombre());
+
+console.log(circulo.obtenerColor());
