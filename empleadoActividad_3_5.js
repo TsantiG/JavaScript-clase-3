d48dba4a2f5639d8509acd532a057cfe8da115cb
TsantiG <santigutierrez081@gmail.com>
2024-08-03 14:04:37 -0500

    Polimorfismo - Implementación en Clase Empleado por TsantiG

---
+++ empleadoActividad_3_5.js
@@ -0,0 +1,10 @@
+const Trabajador = require('./interfaceActividad3');
+
+class Empleado extends Trabajador{
+    realizarTarea() {
+        console.log("Trabajando en la oficina.");
+    }
+
+
+    
+}
