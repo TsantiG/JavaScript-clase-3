048888f4f6445896ac22d36faa897f0b83960da3
TsantiG <santigutierrez081@gmail.com>
2024-08-03 15:48:50 -0500

    Herencia y Polimorfismo - Implementación del Método calcularSalario() en Clase Vendedor por TsantiG

--- empleadoActividad_3_5.js
+++ empleadoActividad_3_5.js
@@ -38,7 +38,7 @@ class Empleado extends Trabajador{
 
 
 }
-
+// ---------------------------------------------------1--------------------
 class Gerente extends Empleado{
 
     #bono;
@@ -66,7 +66,7 @@ class Gerente extends Empleado{
 const felipe = new Gerente("felipe", "Perez", 30, 1, 35);
 console.log(felipe.obtenerTodo());
 
-
+// -----------------------------------------------------2-----------------------------
 class Vendedor extends Empleado{
 
     #comision;
@@ -79,11 +79,22 @@ class Vendedor extends Empleado{
         return this.#comision;
     }
 
+    calcularSalario(){
+        
+        return this.obtenerSalario()+this.#comision;
+    }
 
-    
+    obtenerTodo(){
+        return `El gerente ${this.obtenerNombre()} con salario base de ${this.obtenerSalario()} recive un pago total de ${this.calcularSalario()} por la comision con la cantidad de ${this.obtenerComision()}`
+    }
 
 }
 
+const andres = new Vendedor("Andres", "Monsalve", 25, 1, 45);
+console.log(andres.obtenerTodo());
+
+
+//------------------------------1-------------------
 class Faltas extends Empleado{
 
     #FaltaTrabajo;
@@ -92,11 +103,21 @@ class Faltas extends Empleado{
         this.#FaltaTrabajo=faltaTrabajo;
     }
 
-    obtenerBono(){
+    obtenerFalta(){
         return this.#FaltaTrabajo;
     }
 
 
-    
+    calcularSalario(){
+        
+        return this.obtenerSalario()-this.#FaltaTrabajo;
+    }
+
+    obtenerTodo(){
+        return `El gerente ${this.obtenerNombre()} con salario base de ${this.obtenerSalario()} recive un pago total de ${this.calcularSalario()} por el bono con la cantidad de ${this.obtenerFalta()}`
+    }
 
 }
+
+const dayan = new Faltas("Dayan", "Berrio", 30, 1, 35);
+console.log(dayan.obtenerTodo());
