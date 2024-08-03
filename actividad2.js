4ff9c11e0715647314460011b99a94649250c938
TsantiG <santigutierrez081@gmail.com>
2024-08-03 12:32:54 -0500

    Herencia - Implementación de Propiedad adicional para el grado por TsantiG

--- actividad2.js
+++ actividad2.js
@@ -1,18 +1,26 @@
 const Persona = require('./actividad1'); // no podia importar, mejor usar esto.
 
 class Estudiante extends Persona {
-
+    #grado; 
 
     constructor(nombre, edad, fechaNacimiento, grado){
         super(nombre, edad, fechaNacimiento); // acuerdate que este es lo que se hereda.
-
+        this.#grado = grado;
     }
 
 
+    establecerGrado(grado) {
+        this.#grado = grado;
+    }
 
+    obtenerGrado(){
+        return this.#grado;
+    }
     
 
 }
 
-const santiago = new Estudiante("santi", 17, "2001-04-17",);
+const santiago = new Estudiante("santi", 17, "2001-04-17", "Avanzado");
+
+console.log(santiago.obtenerGrado());
 
